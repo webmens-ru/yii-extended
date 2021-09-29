7041c8d80210c64c06587c93f0c24a75ef4c06e4
webmans-ru <lukoyanov_aa@webmens.ru>
2021-09-29 21:02:17 +0300

    +

---
+++ src/Hw.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace wm\yii;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Hw
+ *
+ * @author Александр
+ */
+class Hw
+{
+    
+    public static function test(){
+        return 'test';
+    }
+    //put your code here
+}
