b95a2a9110df2cf3b8768c2c58e3a5b2d4389266
RashnFlow <62694562+RashnFlow@users.noreply.github.com>
2022-10-28 12:18:15 +0300

    fix: Возможность передать id для удаления: - В теле запроса - В query-параметрах

---
+++ src/rest/DeleteAction.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @link https://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license https://www.yiiframework.com/license/
+ */
+
+namespace wm\yii\rest;
+
+use Yii;
+use yii\rest\Action;
+use yii\web\ServerErrorHttpException;
+
+/**
+ * DeleteAction implements the API endpoint for deleting a model.
+ *
+ * For more details and usage information on DeleteAction, see the [guide article on rest controllers](guide:rest-controllers).
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class DeleteAction extends Action
+{
+    /**
+     * Deletes a model.
+     * @param mixed $id id of the model to be deleted.
+     * @throws ServerErrorHttpException on failure.
+     */
+    public function run()
+    {
+        $request = Yii::$app->request;
+        Yii::warning($request->post(), 'post');
+        if ($getId = $request->get('id')) {
+            $id = $getId;
+        } elseif ($postId = $request->post('id')) {
+            $id = $postId;
+        }
+        $model = $this->findModel($id);
+
+        if ($this->checkAccess) {
+            call_user_func($this->checkAccess, $this->id, $model);
+        }
+
+        if ($model->delete() === false) {
+            throw new ServerErrorHttpException('Failed to delete the object for unknown reason.');
+        }
+
+        Yii::$app->getResponse()->setStatusCode(204);
+    }
+}
