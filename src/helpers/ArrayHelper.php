053fecfc946d4279840f2a39b985c55ad1eaeca4
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2022-12-05 11:07:05 +0300

    feat(ArrayHelper): Добавлена функция getDiff($old, $new) которая находит расхождение массивов или объектов.

---
+++ src/helpers/ArrayHelper.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+use yii\helpers\BaseArrayHelper;
+
+namespace wm\yii\helpers;
+
+use yii\base\BaseObject;
+
+/**
+ * ArrayHelper provides additional array functionality that you can use in your
+ * application.
+ *
+ * For more details and usage information on ArrayHelper, see the [guide article on array helpers](guide:helper-array).
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ArrayHelper extends BaseArrayHelper
+{
+    /**
+     * @param array | BaseObject $old
+     * @param array | BaseObject $new
+     * @return array
+     */
+    public static function getDiff($old, $new){
+        $arrayOld = ArrayHelper::toArray($old);
+        $arrayNew = ArrayHelper::toArray($new);
+
+        $newKeys = [];
+        $deleteKeys = [];
+        $diff = [];
+
+        foreach ($arrayOld as $key => $value){
+            if(array_key_exists($key, $arrayNew)){
+                if($arrayOld[$key] != $arrayNew[$key]){
+                    $diff[$key] = ['old' => $arrayOld[$key], 'new' => $arrayNew[$key]];
+                }
+                unset($arrayNew[$key]);
+            }else{
+                $deleteKeys[$key] = $value;
+            }
+        }
+        $newKeys = $arrayNew;
+
+        return [
+            'newKeys'=> $newKeys,
+            'deleteKeys' => $deleteKeys,
+            'diff' => $diff,
+        ];
+    }
+
+}
