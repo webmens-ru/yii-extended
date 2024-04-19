f475eb3cabb82a9564a355dbd09adc63dfb0f0ad
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2024-04-19 15:04:29 +0300

    feat:() Добавлен класс помощник по работе со временем

---
+++ src/helpers/TimeHelper.php
@@ -0,0 +1,38 @@
+<?php
+namespace wm\yii\helpers;
+
+use yii\base\BaseObject;
+
+
+/**
+ *
+ */
+class TimeHelper extends BaseObject
+{
+
+    /**
+     * @param int $secs
+     * @param string[] $hourStrings
+     * @param string[] $minuteStrings
+     * @return string
+     */
+    public static function secToStr(
+        $secs,
+        $hourStrings = ['час', 'часа', 'часов'],
+        $minuteStrings = ['минута', 'минуты', 'минут']
+    )
+    {
+        $res = '';
+        $hours = floor($secs / 3600);
+        $secs = $secs % 3600;
+        $res .= TextHelper::num_word($hours, $hourStrings) . ', ';
+
+        $minutes = floor($secs / 60);
+        $res .= TextHelper::num_word($minutes, $minuteStrings);
+
+//        $res .= self::num_word($secs, array('секунда', 'секунды', 'секунд'));
+
+        return $res;
+    }
+
+}
