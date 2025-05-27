bd4529a2da1084f839908a31ad24a75e484938e7
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2025-05-27 07:16:19 +0300

    fix исправлена ошибка в getSubUsers

--- src/helpers/B24Helper.php
+++ src/helpers/B24Helper.php
@@ -13,9 +13,27 @@ use wm\b24tools\b24Tools;
 
 class B24Helper
 {
-    public static function getSubUsers($b24UserId, $admins = [])
+    /**
+     * @param int $b24UserId
+     * @param int[] $adminUsers
+     * @return mixed[]
+     * @throws \Bitrix24\Exceptions\Bitrix24ApiException
+     * @throws \Bitrix24\Exceptions\Bitrix24EmptyResponseException
+     * @throws \Bitrix24\Exceptions\Bitrix24Exception
+     * @throws \Bitrix24\Exceptions\Bitrix24IoException
+     * @throws \Bitrix24\Exceptions\Bitrix24MethodNotFoundException
+     * @throws \Bitrix24\Exceptions\Bitrix24PaymentRequiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalDeletedException
+     * @throws \Bitrix24\Exceptions\Bitrix24PortalRenamedException
+     * @throws \Bitrix24\Exceptions\Bitrix24SecurityException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsExpiredException
+     * @throws \Bitrix24\Exceptions\Bitrix24TokenIsInvalidException
+     * @throws \Bitrix24\Exceptions\Bitrix24WrongClientException
+     * @throws \yii\base\Exception
+     * @throws \yii\db\Exception
+     */
+    public static function getSubUsers($b24UserId, $adminUsers = [])
     {
-        $adminUsers = explode(",", $admins);
         $component = new b24Tools();
         $b24App = $component->connectFromAdmin();
         $obB24 = new B24Object($b24App);
